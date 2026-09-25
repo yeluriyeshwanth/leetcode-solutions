c1ca122ec2f63e771282653a5799713ce2a95bc4
Yeshwanth Yeluri <yeluriyeshwanth@gmail.com>
2026-09-25 14:59:57 +0530

    Add Easy Java Solution for 88. Merge Sorted Array from LeetCode

--- LeetCode/Java/Easy/88. Merge Sorted Array/README.md
+++ LeetCode/Java/Easy/88. Merge Sorted Array/README.md
@@ -8,8 +8,8 @@
 Array, Two Pointers, Sorting
 
 ### 🚀 Performance
-- **Runtime:** 4 ms
-- **Memory:** 43.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Two Pointers/88. Merge Sorted Array/Solution.java
+++ LeetCode/Patterns/Two Pointers/88. Merge Sorted Array/Solution.java
@@ -1,9 +1,30 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-         for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
-        }
-
-        Arrays.sort(nums1);
+         int[] result = new int[m+n];
+         int i=0;
+         int j = 0;
+         int k = 0;
+         while(i<m && j<n)
+         {
+            if(nums1[i]<nums2[j])
+            {
+                result[k++]=nums1[i++];
+            }
+            else{
+                result[k++]=nums2[j++];
+            }
+         }
+         while(i<m)
+         {
+            result[k++]=nums1[i++];
+         }
+         while(j<n)
+         {
+            result[k++]=nums2[j++];
+         }
+         for(int h=0;h<m+n;h++)
+         {
+            nums1[h]=result[h];
+         }
     }
 }
